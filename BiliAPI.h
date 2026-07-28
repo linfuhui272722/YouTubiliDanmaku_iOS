8e932113727cc66231b4ad4b0e928176007a8cf5
linfuhui272722 <161229783+linfuhui272722@users.noreply.github.com>
2026-07-28 12:15:05 +0800

    Update BiliAPI.h to include token in methods

--- BiliAPI.h
+++ BiliAPI.h
@@ -4,13 +4,15 @@
 //
 //  Bilibili API 客户端
 //  - 搜索视频
-//  - 获取视频信息（BVID -> CID）
-//  - 获取弹幕 XML（CID -> 弹幕列表）
-//  - deflate-raw 解压缩
+//  - 获取视频信息（BVID -> CID + token）
+//  - 获取弹幕（CID + token -> 弹幕列表）
+//  - gzip 解压缩
 //
 
 #import <Foundation/Foundation.h>
 #import "DanmakuModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface BiliAPI : NSObject
 
@@ -23,37 +25,41 @@
  * @param completion 回调，results 为搜索结果数组
  */
 - (void)searchVideoWithTitle:(NSString *)title
-                     channel:(NSString *)channel
-                  completion:(void (^)(NSArray<BiliSearchResult *> *results, NSError *error))completion;
+                     channel:(nullable NSString *)channel
+                  completion:(void (^)(NSArray<BiliSearchResult *> *results, NSError * _Nullable error))completion;
 
 /**
- * 获取视频信息（BVID -> CID）
+ * 获取视频信息（BVID -> CID, token）
  * @param bvid BV 号
- * @param completion 回调，cid 为弹幕 CID
+ * @param completion 回调，cid 为弹幕 CID，token 为弹幕接口所需 token
  */
 - (void)fetchVideoInfoWithBVID:(NSString *)bvid
-                    completion:(void (^)(NSString *cid, NSString *title, NSTimeInterval duration, NSError *error))completion;
+                    completion:(void (^)(NSString * _Nullable cid, NSString * _Nullable title, NSTimeInterval duration, NSString * _Nullable token, NSError * _Nullable error))completion;
 
 /**
- * 获取弹幕列表（CID -> 弹幕数组）
+ * 获取弹幕列表（CID + token -> 弹幕数组）
  * @param cid 弹幕 CID
+ * @param token 从 view 接口获取的 token
  * @param completion 回调，danmakus 为弹幕数组
  */
 - (void)fetchDanmakuWithCID:(NSString *)cid
-                 completion:(void (^)(NSArray<Danmaku *> *danmakus, NSError *error))completion;
+                      token:(NSString *)token
+                 completion:(void (^)(NSArray<Danmaku *> *danmakus, NSError * _Nullable error))completion;
 
 /**
  * 一步获取弹幕（BVID -> 弹幕数组）
- * 内部先获取 CID，再获取弹幕
+ * 内部先获取 CID 和 token，再获取弹幕
  * @param bvid BV 号
  * @param completion 回调
  */
 - (void)fetchDanmakuForBVID:(NSString *)bvid
-                completion:(void (^)(NSArray<Danmaku *> *danmakus, NSError *error))completion;
+                completion:(void (^)(NSArray<Danmaku *> *danmakus, NSError * _Nullable error))completion;
 
 /**
- * 设置 Cookie（用于绕过 Bilibili 风控）
+ * 设置 Cookie（用于绕过 Bilibili 风控，建议包含 DedeUserID 和 bili_jct）
  */
 @property (nonatomic, copy) NSString *cookie;
 
 @end
+
+NS_ASSUME_NONNULL_END
